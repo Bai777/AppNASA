0b1d3ab5a59da04f7e6890e4a56821fa7e30369c
Stepan <bai_777@mail.ru>
2021-09-07 20:18:29 +0300

    приступил к дз 1 AppNASA

--- app/src/main/java/view/MainActivity.kt
+++ app/src/main/java/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.appnasa
+package view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appnasa.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
