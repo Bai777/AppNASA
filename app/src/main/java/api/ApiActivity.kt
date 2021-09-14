ef41037cadda655580cab8aa61a96a3ad70199e6
Stepan <bai_777@mail.ru>
2021-09-14 19:25:11 +0300

    сделал пролистывание планет с ViewPager

---
+++ app/src/main/java/api/ApiActivity.kt
@@ -0,0 +1,17 @@
+package api
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appnasa.databinding.ActivityApiBinding
+
+class ApiActivity: AppCompatActivity() {
+
+    lateinit var binding: ActivityApiBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityApiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+    }
+}
