b9499674b4a0a3d738e160af06825f2e259b8a69
Stepan <bai_777@mail.ru>
2021-09-18 22:55:32 +0300

    попытка сделать behaviorпадает с ошибкой

---
+++ app/src/main/java/view/example/FABFloatOnScroll.kt
@@ -0,0 +1,46 @@
+package view.example
+
+import android.view.View
+import android.view.animation.LinearInterpolator
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.core.view.ViewCompat
+
+
+
+
+class FABFloatOnScroll : FloatingActionButton.Behavior() {
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int,
+        consumed: IntArray
+    ) {
+
+        if (dyConsumed > 0) {
+            val layoutParams = child.layoutParams as CoordinatorLayout.LayoutParams
+            val fabBottomMargin = layoutParams.bottomMargin
+            child.animate().translationY((child.height + fabBottomMargin).toFloat())
+                .setInterpolator(LinearInterpolator()).start()
+        } else if (dyConsumed < 0) {
+            child.animate().translationY(0F).setInterpolator(LinearInterpolator()).start()
+        }
+    }
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+}
