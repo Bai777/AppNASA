8ec21a21ba1455114c1a1479b87e90dad332c99a
Stepan <bai_777@mail.ru>
2021-09-25 20:22:56 +0300

    сделал рефакторинг

---
+++ app/src/main/java/view/recycler/BaseViewHolder.kt
@@ -0,0 +1,8 @@
+package view.recycler
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    abstract fun bind(data: Data)
+}
